7e5ee64134a9e698ef4066f2b4abf0dc0b3cb4ea
HannahSmirnova <alexamy1986@gmail.com>
2025-09-26 19:26:38 -0600

    results and meanings components

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 
 export default function SearchEngine() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    console.log(apiUrl);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
@@ -29,6 +30,7 @@ export default function SearchEngine() {
         ></input>
         <input type="submit" value="Search"></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
